f3bc94e07058105ceb193ec2ef7b4e4ab0b71c62
Marcelo Barth <marcelo.barth@gmail.com>
2016-11-08 15:28:23 -0800

    Updated to simply publish to topic and consume off topic.

--- consumer.js
+++ consumer.js
@@ -1,48 +1,17 @@
 var path = require('path'),
     debug = require('debug')('kafka-node:sample-consumer'),
-    kafka = require('kafka-node'),
-    Consumer = kafka.Consumer,
-    client = new kafka.Client(),
-    currentTopic = 'twitter',
-    consumer = new Consumer(
-        client,
-        [
-            { topic: currentTopic, partition: 0 }
-        ],
-        {
-            autoCommit: false
-        }
-    );
+    kafkaTopic = 'twitter';
 
-process.on('message', function(message) {
-    debug(path.basename(__filename), 'script received message:', message);
-    var object = JSON.parse(message);
-    if (object.topicName) {
-        consumer.removeTopics([currentTopic], function(err, removed){
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-            debug('removed current topic', currentTopic);
-            debug(removed);
-        });
-        currentTopic = object.topicName;
-        consumer.addTopics([object.topicName], function(err, added){
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-            debug('added new topic', currentTopic);
-            debug(added);
-        });
-    }
-});
+/**
+ * Set up a kafka consumer
+ */
+var KafkaNodeUtil = require('./kafka-node-util');
+var config = {fromOffset: true};
+// using util class that handles retrieving messages from latest offset
+var knu = new KafkaNodeUtil(config);
+knu.listenOnTopic(kafkaTopic, processTweet)
 
-consumer.on('message', function (message) {
-    var tweet = JSON.parse(message.value);
+function processTweet(rawMessageValue) {
+    var tweet = JSON.parse(rawMessageValue);
     debug(tweet);
-});
-
-consumer.on('error', function(err) {
-    console.error('here' ,err);
-});
+}
